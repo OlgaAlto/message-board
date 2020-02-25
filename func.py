07789db52a590e7ce0311b2e6b5ede43daab0dc5
OlgaAlto <61138985+OlgaAlto@users.noreply.github.com>
2020-02-25 18:39:22 +0200

    test

--- func.py
+++ func.py
@@ -1,21 +1,26 @@
 def read(data):
-    ''
     ''' This function should receive a dictionary (see the json file format) and return a list of rows
-    For example: 
+    For example:
     data = {
-            "names": ["Son", "Thao"], 
+            "names": ["Son", "Thao"],
             "posted_at": ["16/02/2020 22:37", "16/02/2020 22:39"],
             "messages": ["Hello", "Hi"]
             }
 
     rows = [
-            ("Son", "16/02/2020 22:37", "Hello"), 
+            ("Son", "16/02/2020 22:37", "Hello"),
             ("Thao", "16/02/2020 22:39", "Hi")
             ]
     Bonus: You might want to show the latest messages first. How would you do that?
     '''
-    # Your code here
+    rows = []
+    name = data[names]
+    post_time = data[posted_at]
+    message = data[messages]
 
+    for name, post_time, message in zip(names, posted_at, messages):
+        rows.append(name, post_time, message)
+        # rows.reverse()
     return rows
 
 
@@ -29,7 +34,7 @@ def write(rows, name, message):
             ]
     name = "Anonymous"
     message = "Hello world"
-    
+
     data = {
             "names": ["Son", "Thao", "Anonymous"], 
             "posted_at": ["16/02/2020 22:37", "16/02/2020 22:39", "<current system date time>"],
@@ -38,8 +43,11 @@ def write(rows, name, message):
     Note also the format of the date time should be the same as in the original data.
     '''
     # Your code here
-
-    return data
+    data = {"names": ["Son", "Thao"],
+            "posted_at": ["16/02/2020 22:37", "16/02/2020 22:39"],
+            "messages": ["Hello", "Hi"]
+            }
+    read(data)
 
 
 def checkLen(rows):
@@ -49,3 +57,10 @@ def checkLen(rows):
     If there are more than 20 in the data file, you should show the 20 latest messages.
     '''
     # Your code here
+    length_of_rows = len(rows)
+    max_of_messages = 20
+    if length_of_rows > max_of_messages:
+        print(rows[-19:])
+    else:
+        print(rows)
+    return length_of_rows
